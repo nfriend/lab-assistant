b2c8be4fdadb0869072e2df0020450ff2c9c0f75
Nathan Friend <nathan@gitlab.com>
2019-08-23 08:27:59 -0300

    Working i18n

--- Lab_Assistant/lambda/custom/i18next-scanner.config.ts
+++ Lab_Assistant/lambda/custom/i18next-scanner.config.ts
@@ -1,9 +1,15 @@
 module.exports = {
   options: {
     debug: true,
+    removeUnusedKeys: true,
+    sort: true,
+
+    // The list of all languages codes supported by this skill
     lngs: ['en'],
+
     defaultLng: 'en',
-    defaultValue: (lng: string, ns: string, key: string) => key,
+    defaultValue: (lng: string, ns: string, key: string) =>
+      lng === 'en' ? key : '',
     nsSeparator: false,
     keySeparator: false,
   },
--- Lab_Assistant/lambda/custom/lib/interceptors/LocalizationInterceptor.ts
+++ Lab_Assistant/lambda/custom/lib/interceptors/LocalizationInterceptor.ts
@@ -1,17 +1,42 @@
 import * as Alexa from 'ask-sdk-core';
 import * as i18n from 'i18next';
 import * as sprintf from 'i18next-sprintf-postprocessor';
+import * as fs from 'fs';
+import * as util from 'util';
+import * as path from 'path';
 
-const translations = {
-  en: require('../../i18n/en/translation.json'),
-};
+const readdirAsync = util.promisify(fs.readdir);
+
+const I18N_DIR = path.resolve(__dirname, '../../../i18n');
+
+/** A promise that returns an object containing all available translations */
+const translationsPromise = (async () => {
+  const langs = (await readdirAsync(I18N_DIR, {
+    withFileTypes: true,
+  }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+  const translations: { [lang: string]: any } = {};
+
+  langs.forEach(l => {
+    translations[l] = {
+      translation: require(path.resolve(I18N_DIR, l, 'translation.json')),
+    };
+  });
+
+  return translations;
+})();
 
 export const LocalizationInterceptor: Alexa.RequestInterceptor = {
   async process(handlerInput) {
     await i18n.use(sprintf).init({
       lng: handlerInput.requestEnvelope.request.locale,
       fallbackLng: 'en',
-      resources: translations,
+      resources: await translationsPromise,
+      returnEmptyString: false,
+      keySeparator: false,
+      nsSeparator: false,
     });
   },
 };
