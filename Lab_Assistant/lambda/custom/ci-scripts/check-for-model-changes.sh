1acc1e062245733858f9f1364ede3876b98f1ad9
Nathan Friend <nathan@gitlab.com>
2019-09-03 15:08:15 -0300

    Add linting of grammar

---
+++ Lab_Assistant/lambda/custom/ci-scripts/check-for-model-changes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Generate a new skill model
+npm run model
+
+# Run "git status", because for some reason the command
+# below doesn't correct detect changes otherwise ¯\＿(ツ)＿/¯
+git status
+
+if git diff-index --quiet HEAD --; then
+    echo -e "\033[32;1mThe model file is up to date 👍\033[0m"
+    exit 0
+else
+    echo -e "\033[31;1m⚠️  The model has been changed! Please run \"npm run model\" and commit these changes. ⚠️\033[0m"
+    exit 1
+fi
