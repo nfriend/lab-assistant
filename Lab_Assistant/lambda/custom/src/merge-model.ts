7a4e4aefcdc83cfd092eee232ee36c9e0cbbf430
Nathan Friend <nathan@gitlab.com>
2019-09-04 09:21:41 -0300

    Start to add dialog support for pipelines

---
+++ Lab_Assistant/lambda/custom/src/merge-model.ts
@@ -0,0 +1,20 @@
+// A utility script that merges en-US.base.json into en-US.json.
+// This is necessary because Alexa Skill Utterance and Schema Generator
+// (https://github.com/KayLerch/alexa-utterance-generator)
+// doesn't yet support dialogs.
+
+import * as fs from 'fs';
+import * as _ from 'lodash';
+import * as path from 'path';
+
+const basePath = '../../models';
+
+const base = require(path.resolve(basePath, 'en-US.base.json'));
+const model = require(path.resolve(basePath, 'en-US.json'));
+
+const merged = _.defaultsDeep({}, base, model);
+
+fs.writeFileSync(
+  path.resolve(basePath, 'en-US.json'),
+  JSON.stringify(merged, null, 2),
+);
