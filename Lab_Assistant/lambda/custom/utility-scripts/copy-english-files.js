af314f7d795264fea3304178ddaa13478fc00e2a
Nathan Friend <nathan@gitlab.com>
2019-09-16 15:17:48 -0300

    Also copy privacy policy and TOS links

--- Lab_Assistant/lambda/custom/utility-scripts/copy-english-files.js
+++ Lab_Assistant/lambda/custom/utility-scripts/copy-english-files.js
@@ -20,14 +20,17 @@ supportedEnglishCodes.forEach(code => {
   fs.writeFileSync(newFilePath, JSON.stringify(enUsModelJson, null, 2));
 });
 
-// Copy the manifest.publishingInformation.locales.en-US section in skill.json
-// to en-CA, en-GB, etc.
+// Copy the manifest.publishingInformation.locales.en-US and
+// manifest.privacyAndCompliance.locales.en-US sections in
+// skill.json to en-CA, en-GB, etc.
 const skillJsonPath = path.resolve(__dirname, '../../../skill.json');
 const skillJson = require(skillJsonPath);
 
-const enUsLocale = skillJson.manifest.publishingInformation.locales['en-US'];
+const enUsPublishing = skillJson.manifest.publishingInformation.locales['en-US'];
+const enUsPrivacy = skillJson.manifest.privacyAndCompliance.locales['en-US'];
 supportedEnglishCodes.forEach(code => {
-  skillJson.manifest.publishingInformation.locales[code] = enUsLocale;
+  skillJson.manifest.publishingInformation.locales[code] = enUsPublishing;
+  skillJson.manifest.privacyAndCompliance.locales[code] = enUsPrivacy;
 });
 
 fs.writeFileSync(skillJsonPath, JSON.stringify(skillJson, null, 2));
